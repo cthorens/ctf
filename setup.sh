3e7b57da91e4088003eb01c268c36dec0c62fb5f
cthorens <cthorens@student.ethz.ch>
2022-12-10 15:28:18 +0100

    delete scritps

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# export PYSPARK_PYTHON=/usr/bin/python3.7
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
-export PYTHONPATH="/home/clement/Documents/Master/thesis/mthesis-clementthorens/enforce"
-# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-source venv/bin/activate
